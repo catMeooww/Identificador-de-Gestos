17020057e31c320ddc0dfd2916c86bd7f9607f6c
catMeooww <110690475+catMeooww@users.noreply.github.com>
2023-05-02 15:50:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -25,3 +25,36 @@ var Speech = window.speechSynthesis;
 var UtterThis = new SpeechSynthesisUtterance(P1 + P2);
 Speech.speak(UtterThis);
 }
+function Check(){
+    var Img = document.getElementById("Gesto");
+    classifier.classify(Img,gotResults)
+}
+function gotResults(error, results){
+    if (error) {
+        console.error(error);
+    }else{
+        console.log(results);
+        Previsao1 = results[0].label;
+        Previsao2 = results[1].label;
+        document.getElementById("ResultName").innerHTML = Previsao1;
+        document.getElementById("ResultName2").innerHTML = Previsao2;
+        if (Previsao1 == "Tranquilo"){
+          document.getElementById("Emoji").innerHTML = "&#129305;"
+        }
+        if (Previsao1 == "Legal"){
+            document.getElementById("Emoji").innerHTML = "&#128077;"
+        }
+        if (Previsao1 == "Vitória"){
+            document.getElementById("Emoji").innerHTML = "&#9996;"
+        }
+        if (Previsao2 == "Tranquilo"){
+            document.getElementById("Emoji2").innerHTML = "&#129305;"
+          }
+          if (Previsao2 == "Legal"){
+              document.getElementById("Emoji2").innerHTML = "&#128077;"
+          }
+          if (Previsao2 == "Vitória"){
+              document.getElementById("Emoji2").innerHTML = "&#9996;"
+          }
+    }
+}
